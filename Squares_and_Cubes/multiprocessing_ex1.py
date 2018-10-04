4ecd54c8045e237a3357c014376bca1cb63d1422
alok <alok760@gmail.com>
2018-10-04 13:50:31 +0530

    changes

--- Squares_and_Cubes/multiprocessing_ex1.py
+++ Squares_and_Cubes/multiprocessing_ex1.py
@@ -3,16 +3,15 @@ import multiprocessing
 
 def calc_square(numbers):
     for n in numbers:
-        time.sleep(5)
-        print('square' + str(n*n))
+        print('square' + str(n*n*n))
 
 def calc_cube(numbers):
     for n in numbers:
-        time.sleep(5)
         print('cube' + str(n*n*n))
 
 if __name__ == "__main__":
-    arr = [2,3,8,9]
+    t = time.time()
+    arr = [2999999999999,999999999399,9999999999999,99999,19999999992,39994,39994,9923,1992,9923,999912,99923,1992,99912,9934,945,923,1992,234,9912,23,34,12,34,234,235,2663,2333,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992,19999999992]
     p1 = multiprocessing.Process(target=calc_square,args=(arr,))
     p2 = multiprocessing.Process(target=calc_cube,args=(arr,))
 
@@ -21,5 +20,5 @@ if __name__ == "__main__":
 
     p1.join()
     p2.join()
-
+    print("time taken : ", time.time()-t)
     print("done!!")
---
+++ Squares_and_Cubes/normal1.py
@@ -0,0 +1,7 @@
+import multiprocessing
+def fib(n):
+    if n==1 or n==0:
+        return 1
+
+    else:
+        return fib(n-1)+fib(n-2)
